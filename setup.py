e0076b2002eb4918ece49eaeb01029c9f1c27b2b
co1 <gunaratnecs@ornl.gov>
2023-06-14 22:10:53 -0400

    Version fix

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SuperNeuroABM",
+    version="0.0.1",
+    author="Chathika Gunaratne, Prasanna Date, Shruti Kulkarni",
+    author_email="gunaratnecs@ornl.gov",
+    packages=["superneuroabm"],
+    include_package_data=True,
+    url="https://code.ornl.gov/superneuro/superneuroabm",
+    license="GPL",
+    description="A GPU-based multi-agent simulation framework for neuromorphic computing.",
+    long_description="""A GPU-based multi-agent simulation framework for neuromorphic computing.""",
+    long_description_content_type="text/markdown",
+    project_urls={"Source": "https://github.com/ORNL/superneuroabm"},
+    install_requires=["numba==0.55.1", "numpy==1.21.6", "tqdm==4.64.1"],
+)
