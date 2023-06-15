aec1f15135be2f8be98c4dd72327c57c3f3c0a01
co1 <gunaratnecs@ornl.gov>
2023-06-14 21:58:06 -0400

    Core libs

---
+++ superneuroabm/core/model.py
@@ -0,0 +1,226 @@
+"""
+SuperNeuroABM basic Model class
+
+"""
+from typing import Dict, List, Callable, Set, Any
+import math
+import heapq
+from multiprocessing import Pool, Manager
+import inspect
+
+from numba import cuda
+from numba.cuda.random import create_xoroshiro128p_states
+from tqdm import tqdm
+
+from sagesim.agent import AgentFactory, Breed
+
+
+class Model:
+    THREADSPERBLOCK = 32
+
+    def __init__(self, name: str = "Untitled", use_cuda: bool = True) -> None:
+        self._name = name
+        self._agent_factory = AgentFactory()
+        self._use_cuda = use_cuda
+        if self._use_cuda:
+            if not cuda.is_available():
+                raise EnvironmentError(
+                    "CUDA requested but no cuda installation detected."
+                )
+            else:
+                pass
+                # device = cuda.get_current_device()
+                # device.reset()
+
+    def register_breed(self, breed: Breed) -> None:
+        if self._agent_factory.num_agents > 0:
+            raise Exception(
+                f"Breeds must be registered before agents are created!"
+            )
+        self._agent_factory.register_breed(breed)
+
+    def create_agent_of_breed(self, breed: Breed, **kwargs) -> int:
+        agent_id = self._agent_factory.create_agent(breed, **kwargs)
+        return agent_id
+
+    def get_agent_info(self, breed: Breed, id: int) -> List[Any]:
+        """
+        Query agent from population by its breed and id
+
+        """
+        raise NotImplemented()
+
+    def get_agent_property_value(self, id: int, property_name: str) -> Any:
+        return self._agent_factory.get_agent_property_value(
+            property_name=property_name, agent_id=id
+        )
+
+    def set_agent_property_value(
+        self,
+        id: int,
+        property_name: str,
+        value: Any,
+        dims: List[int] = None,
+    ) -> None:
+        self._agent_factory.set_agent_property_value(
+            property_name=property_name, agent_id=id, value=value, dims=dims
+        )
+
+    def get_agents_with(self, query: Callable) -> Set[List[Any]]:
+        return self._agent_factory.get_agents_with(query=query)
+
+    def setup(self) -> None:
+        # Create record of agent step functions by breed and priority
+        self._breed_idx_2_step_func_by_priority: List[Dict[int, Callable]] = []
+        heap_priority_breedidx_func = []
+        for breed in self._agent_factory.breeds:
+            for priority, func in breed.step_funcs.items():
+                heap_priority_breedidx_func.append(
+                    (priority, (breed._breedidx, func))
+                )
+        heapq.heapify(heap_priority_breedidx_func)
+        last_priority = None
+        while heap_priority_breedidx_func:
+            priority, breed_idx_func = heapq.heappop(
+                heap_priority_breedidx_func
+            )
+            if last_priority == priority:
+                # same slot in self._breed_idx_2_step_func_by_priority
+                self._breed_idx_2_step_func_by_priority[-1].update(
+                    {breed_idx_func[0]: breed_idx_func[1]}
+                )
+            else:
+                # new slot
+                self._breed_idx_2_step_func_by_priority.append(
+                    {breed_idx_func[0]: breed_idx_func[1]}
+                )
+                last_priority = priority
+
+        # Generate agent data tensor
+        self._agent_data_tensors = (
+            self._agent_factory.generate_agent_data_tensors(self._use_cuda)
+        )
+
+    def simulate(
+        self, ticks: int, update_data_ticks: int = 1, num_cpu_proc: int = 4
+    ) -> None:
+        # Generate global data tensor
+        self._global_data_vector = [0]  # index 0 reserved for step
+        if not self._use_cuda:
+            with Pool(num_cpu_proc) as pool:
+                with Manager() as manager:
+                    shared_global_data_vector = manager.list(
+                        self._global_data_vector
+                    )
+                    breed_ids = self._agent_data_tensors[0]
+                    jobs = []
+                    for (
+                        breed_idx_2_step_func
+                    ) in self._breed_idx_2_step_func_by_priority:
+                        jobs_in_priority = [
+                            (
+                                breed_idx_2_step_func[breed_ids[agent_id]],
+                                (
+                                    shared_global_data_vector,
+                                    *self._agent_data_tensors,
+                                    agent_id,
+                                ),
+                            )
+                            for agent_id in range(
+                                self._agent_factory._num_agents
+                            )
+                            if breed_ids[agent_id] in breed_idx_2_step_func
+                        ]
+                        jobs.append(jobs_in_priority)
+
+                    for tick in tqdm(
+                        range(ticks),
+                        desc="Simulation Progress",
+                        unit="Tick",
+                        unit_scale=True,
+                        unit_divisor=1000,
+                        dynamic_ncols=True,
+                    ):
+                        shared_global_data_vector[0] = tick
+                        for jobs_in_priority in jobs:
+                            _ = list(map(smap, jobs_in_priority))
+        else:
+            blockspergrid = int(
+                math.ceil(
+                    self._agent_factory.num_agents / Model.THREADSPERBLOCK
+                )
+            )
+            step_funcs_code_obj = generate_gpu_func(
+                len(self._agent_data_tensors),
+                self._breed_idx_2_step_func_by_priority,
+                ticks,
+            )
+            ################
+            shared_global_data_vector = [0]
+            device_global_data_vector = cuda.to_device(
+                shared_global_data_vector
+            )
+            exec(step_funcs_code_obj)
+
+        self._agent_factory.update_agents_properties(
+            self._agent_data_tensors, self._use_cuda
+        )
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+
+def smap(func_args):
+    return func_args[0](*func_args[1])
+
+
+def generate_gpu_func(
+    n_properties,
+    breed_idx_2_step_func_by_priority,
+    ticks,
+):
+    args = [f"a{i}" for i in range(n_properties)]
+    sim_loop = ""
+    for breed_idx_2_step_func in breed_idx_2_step_func_by_priority:
+        for breedidx, breed_step_func in breed_idx_2_step_func.items():
+            step_source = inspect.getsource(breed_step_func)
+            step_func_name = getattr(
+                breed_step_func, "__name__", repr(callable)
+            )
+            step_source = "\t\t\t\t".join(step_source.splitlines(True))
+            sim_loop += f"""
+            \n\t\t\tif breed_id == {breedidx}:
+            \n\t\t\t\t{step_source} 
+            \t{step_func_name}(
+            \t\tdevice_global_data_vector,
+            \t\t{','.join(args)},
+            \t\tagent_id,
+            \t)
+            g.sync()
+            #cuda.syncthreads()
+                
+            """
+    func = f"""
+    \ndef stepfunc(
+    device_global_data_vector,
+    {','.join(args)},
+    ):
+        thread_id = int(cuda.grid(1))
+        if thread_id >= len(a0):
+            return
+        agent_id = thread_id
+        g = cuda.cg.this_grid()
+        breed_id = a0[agent_id]
+        for tick in range({ticks}):
+            device_global_data_vector[0] = tick
+            {sim_loop}
+    \nstepfunc = cuda.jit(stepfunc)
+    \nstepfunc[blockspergrid, Model.THREADSPERBLOCK](
+        device_global_data_vector,
+        *self._agent_data_tensors,
+    )
+    \ncuda.synchronize()
+    """
+    func = func.replace("\t", "    ")
+    return compile(func, "<string>", "exec")
---
+++ superneuroabm/core/util.py
@@ -0,0 +1,63 @@
+import time
+from typing import Iterable, List, Any, Tuple, Optional
+
+import numpy as np
+
+
+def convert_to_equal_side_tensor(
+    tensor: List[Any], max_dims: Optional[Tuple[int]] = None
+) -> np.array:
+    dim2maxlen = {}
+
+    def find_max_depth(l, curr_depth=0):
+        dim2maxlen[curr_depth] = max(dim2maxlen.get(curr_depth, 0), len(l))
+        max_depth = curr_depth
+        for item in l:
+            if type(item) == list:
+                max_depth = max(
+                    max_depth, find_max_depth(item, curr_depth + 1)
+                )
+        return max_depth
+
+    start = time.time()
+    if not max_dims:
+        print("no dims specified... running find max depth")
+        find_max_depth(tensor)
+        max_dims = tuple(list(dim2maxlen.values()))
+    answer = np.full(shape=max_dims, fill_value=np.nan)
+
+    def fill_arr(arr, coord):
+        if len(coord) == len(max_dims):
+            if type(arr) == list:
+                raise TypeError()
+            answer[tuple(coord)] = arr
+        else:
+            if type(arr) != list:
+                raise TypeError()
+            for i, item in enumerate(arr):
+                new_coord = coord + [i]
+                fill_arr(item, new_coord)
+
+    start = time.time()
+    fill_arr(tensor, [])
+    return answer
+
+
+def compress_tensor(arr: Iterable, outer: bool = True):
+    if not hasattr(arr, "__iter__") and not hasattr(
+        arr, "__cuda_array_interface__"
+    ):
+        if not np.isnan(arr):
+            return arr
+        else:
+            return None
+    else:
+        new_arr = []
+        for item in arr:
+            new_item = compress_tensor(item, False)
+            if new_item != None:
+                new_arr.append(new_item)
+        if len(new_arr):
+            return new_arr
+        else:
+            return [] if outer else None
