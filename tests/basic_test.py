99f2cd1d2c3384dc7e7f8960c5c8e9b99c216611
chathika <chathikagunaratne@gmail.com>
2023-08-05 14:19:33 -0400

    Unittests for save load, setup and run with pretrained weights added

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -20,7 +20,7 @@ class BasicTest(unittest.TestCase):
     def test_consecutive_simulate_calls(self):
         """Tests simulate calls"""
 
-        model = NeuromorphicModel(use_cuda=True)
+        model = NeuromorphicModel()
         # Create neurons
         input = model.create_neuron(threshold=0.0)
         output = model.create_neuron(threshold=0.0)
@@ -30,13 +30,13 @@ class BasicTest(unittest.TestCase):
             pre_neuron_id=input, post_neuron_id=output, weight=1.0
         )
 
+        # Setup and simulate
+        model.setup(output_buffer_len=10, use_cuda=True)
         spikes = {1: [(input, 1)], 6: [(input, 100)]}
         for time in spikes:
             for neuron, value in spikes[time]:
                 model.spike(neuron_id=neuron, tick=time, value=value)
 
-        # Setup and simulate
-        model.setup(output_buffer_len=10)
         model.simulate(ticks=6)
         print(model.summary())
         assert model._global_data_vector[0] == 6, "Ticks are off"
@@ -44,8 +44,8 @@ class BasicTest(unittest.TestCase):
             2
         ], "Simulation did not continue properly"
 
-        print(model._agent_factory._property_name_2_agent_data_tensor)
         model.simulate(ticks=4)
+        print(model.summary())
         assert (
             model._global_data_vector[0] == 10
         ), "Ticks didn't continue correctly on second simulate call"
@@ -53,13 +53,11 @@ class BasicTest(unittest.TestCase):
             2,
             7,
         ], "Simulation did not continue properly"
-        print(model._agent_factory._property_name_2_agent_data_tensor)
-        print(model.summary())
 
     def test_consecutive_setup_and_simulate_calls(self):
         """Tests setup and simulate calls"""
 
-        model = NeuromorphicModel(use_cuda=True)
+        model = NeuromorphicModel()
         # Create neurons
         input = model.create_neuron(threshold=0.0)
         output = model.create_neuron(threshold=0.0)
@@ -68,14 +66,13 @@ class BasicTest(unittest.TestCase):
         model.create_synapse(
             pre_neuron_id=input, post_neuron_id=output, weight=1.0
         )
-
+        # Setup and simulate
+        model.setup(output_buffer_len=10, use_cuda=True)
         spikes = {1: [(input, 1)], 6: [(input, 100)]}
         for time in spikes:
             for neuron, value in spikes[time]:
                 model.spike(neuron_id=neuron, tick=time, value=value)
 
-        # Setup and simulate
-        model.setup(output_buffer_len=10)
         model.simulate(ticks=6)
         print(model.summary())
         assert model._global_data_vector[0] == 6, "Ticks are off"
@@ -83,21 +80,20 @@ class BasicTest(unittest.TestCase):
             2
         ], "Simulation did not execute properly"
 
-        # model.setup(output_buffer_len=10)
-        print(model._global_data_vector)
-        print(model._agent_factory._property_name_2_agent_data_tensor)
-
-        model.setup(output_buffer_len=10)
+        model.setup(output_buffer_len=10, use_cuda=True)
+        spikes = {1: [(input, 1)], 6: [(input, 100)]}
+        for time in spikes:
+            for neuron, value in spikes[time]:
+                model.spike(neuron_id=neuron, tick=time, value=value)
         model.simulate(ticks=4)
+        print(model.summary())
         assert (
             model._global_data_vector[0] == 4
         ), "Ticks didn't continue correctly on second simulate call"
+
         assert model.get_spikes(output) == [
             2,
         ], "Simulation did not reset properly"
-        print(model._global_data_vector)
-        print(model._agent_factory._property_name_2_agent_data_tensor)
-        print(model.summary())
 
     def test_breeds(self):
         """Tests multi-breed"""
@@ -109,21 +105,25 @@ class BasicTest(unittest.TestCase):
                 synapse_with_stdp_step_func,
             ],
         }
-        model = NeuromorphicModel(
-            use_cuda=True, neuron_breed_info=neuron_breed_info
-        )
+        model = NeuromorphicModel(neuron_breed_info=neuron_breed_info)
         # Create neurons
         input_0 = model.create_neuron(breed="NeuronSTDPForward", threshold=0.0)
         input_1 = model.create_neuron(breed="Neuron", threshold=0.0)
-        output_2 = model.create_neuron(threshold=2.0)
+        output_2 = model.create_neuron(breed="Neuron", threshold=2.0)
 
         # Create synapses
         model.create_synapse(
-            pre_neuron_id=input_0, post_neuron_id=output_2, weight=1.0
+            pre_neuron_id=input_0,
+            post_neuron_id=output_2,
+            weight=1.0,
+            synapse_learning_params=[3, 0.6, 0.3, 8, 5, 0.8, 1000],
         )
         model.create_synapse(
             pre_neuron_id=input_1, post_neuron_id=output_2, weight=1.0
         )
+
+        # Setup and simulate
+        model.setup(output_buffer_len=10, use_cuda=True)
 
         spikes = {
             1: [
@@ -143,16 +143,84 @@ class BasicTest(unittest.TestCase):
                 (input_1, 1),
             ],  # Input: (1, 1); Expected output: Spike at time 8
         }
-        expected_times = [8]
         for time in spikes:
             for neuron, value in spikes[time]:
                 model.spike(neuron_id=neuron, tick=time, value=value)
 
-        # Setup and simulate
-        model.setup(output_buffer_len=10)
         model.simulate(ticks=10)
         print(model.summary())
         assert (
             round(model.get_synapse_weight(0, 2)) != 1
         ), "Breed had no effect"
         assert model.get_synapse_weight(1, 2) == 1, "Breed had no effect"
+
+    def test_save_load(self):
+        neuron_breed_info = {
+            "Neuron": [neuron_step_func, synapse_step_func],
+            "NeuronSTDPForward": [
+                neuron_step_func,
+                synapse_with_stdp_step_func,
+            ],
+        }
+        model = NeuromorphicModel(neuron_breed_info=neuron_breed_info)
+        # Create neurons
+        input_0 = model.create_neuron(breed="NeuronSTDPForward", threshold=0.0)
+        input_1 = model.create_neuron(breed="Neuron", threshold=0.0)
+        output_2 = model.create_neuron(breed="Neuron", threshold=2.0)
+
+        # Create synapses
+        model.create_synapse(
+            pre_neuron_id=input_0,
+            post_neuron_id=output_2,
+            weight=1.0,
+            synapse_learning_params=[3, 0.6, 0.3, 8, 5, 0.8, 1000],
+        )
+        model.create_synapse(
+            pre_neuron_id=input_1, post_neuron_id=output_2, weight=5.0
+        )
+        model.setup(use_cuda=True, output_buffer_len=10)
+
+        spikes = {
+            7: [
+                (input_0, 1),
+                (input_1, 1),
+            ],  # Expected output: Spike at time 8
+        }
+        for time in spikes:
+            for neuron, value in spikes[time]:
+                model.spike(neuron_id=neuron, tick=time, value=value)
+
+        # Setup and simulate
+        model.simulate(ticks=10)
+        print(model.summary())
+        model.save("model.pickle")
+        model.load("model.pickle")
+        print(model.summary())
+        assert (
+            round(model.get_synapse_weight(input_0, output_2)) == 241
+        ), "STDP enabled synapse failed"
+        assert (
+            model.get_synapse_weight(input_1, output_2) == 5
+        ), "Weight change on STDP disabled synapse"
+
+        model.save("model.pickle")
+        model.load("model.pickle")
+        model.update_synapse(
+            input_0, output_2, synapse_learning_params=[0, 0, 0, 0, 0, 0, 0]
+        )
+        # Setup and simulate
+        model.setup(use_cuda=True, output_buffer_len=10, retain_weights=True)
+        spikes = {
+            3: [
+                (input_0, 1),
+            ],  # Expected output: Spike at time 8
+        }
+        for time in spikes:
+            for neuron, value in spikes[time]:
+                model.spike(neuron_id=neuron, tick=time, value=value)
+
+        model.simulate(ticks=10)
+        print(model.summary())
+        assert model.get_spikes(output_2) == [
+            4
+        ], "Trained and loaded model did not spike correctly"
