27e12774b12914bcf69cd1aef4a433c8e22f72f7
William Fishell <wfishell@login12.frontier.olcf.ornl.gov>
2025-09-26 14:48:11 -0400

    Pushing changes to NMNIST

--- superneuroabm/ssn/NMNIST-test.py
+++ superneuroabm/ssn/NMNIST-test.py
@@ -1,6 +1,7 @@
 import os 
 import numpy as np
 from superneuroabm.model import NeuromorphicModel
+import sagesim
 class Conv2dtSingleLayer:
     def __init__(self,input_x,input_y,SpikeData= None):
         #Spike Data should be organized by spike time
@@ -69,13 +70,51 @@ class Conv2dtSingleLayer:
 
         return events_list
     
-
+    def Add_Output_Channels(Number_Of_Output_Channels): 
+        self.output_layer={}
+        for i in self.ConvKernelList:
+            output_list = []
+            for output in Number_Of_Output_Channels:
+                Output_=self.model.create_soma(
+                    breed='lif_soma',
+                    config_name='config_0',
+                    hyperparameters_overrides={'C':10e-9,'R':1e6,'vthr':-45,'tref':5e-3,'vrest':-60,'vreset':-60,'tref_allows_integration':1,'I_in':0,'scaling_factor':1e-5},
+                    default_internal_state_overrides={'v':-60,'tcount':0.0,'tlast':0.0}
+                    )
+                Synapse= self.model.create_synapse(
+                    breed='single_exp_synapse',
+                    pre_soma_id=i[1],
+                    post_soma_id=Output,
+                    config_name='exp_pair_wise_stdp_config_0',
+                    hyperparameters_overrides={'weight':np.random.uniform(low=50.0, high=100.0),'synpatic_delay':1.0,'scale':1.0,'tau_fall':1e-2,'tau_rise':0},
+                    default_internal_state_overrides={'internal_state':0.0},
+                    learning_hyperparameters_overrides={'stdp_type':10e-3,'tau_pre_stdp':10e-3,'tau_post_stdp':10e-3,'a_exp_pre':0.005,'a_exp_post':0.005,'stdp_history_length':100},
+                    default_internal_learning_state_overrides={'pre_trace':0,'post_trace':0,'dw':0}
+                )
+                output_list.append(Output)
+            self.output_layer[i]=output_list
+        
+    
+    def Convolve_Spike(SpikeCoordinate, Kernel_List_Entry,CurrentSpikeSet, time_step, Stride):
+        #Kernel_List_Entry is an Entry for the kernel we want to convolve on
+        Ranges=[]
+        for i in range(len(Kernel_List_Entry)):
+            for j in range(len(Kernel_List_Entry[0])):
+                Ranges_X=(SpikeCoordinate[0]-i, SpikeCoordinate[0]-i + len(Kernel_List_Entry))
+                Ranges_Y=(SpikeCoordinate[1]-j,SpikeCoordinate[1]-j + len(Kernel_List_Entry[0]))
+                Range = (Ranges_X, Ranges_Y)
+                Ranges.append(Range)
+        for x in range(Ranges[0][0],Ranges[0][1],Stride):
+            for y in range(Ranges[1][0],Ranges[1][1]):
+                coor=(x,y)
+                if coor in CurrentSpikeSet:
+                    kernel_location_x= x - Ranges[0][0]
+                    kernel_location_y = y- Ranges[1][0]
+                    self.model.add_spike(synapse_id=Kernel_List_Entry[kernel_location_x][kernel_location_y],tick=time_step,value=CurrentSpikeSet[coor])
+    
 if __name__=='__main__':
     Model=Conv2dtSingleLayer(2,3)
-    data=Model.Load_File('/home/wf2322/NMNIST/Train/0/59988.bin')
-    print(data[0])
-
-
+  
     Test_Neuron=Model.model.create_soma(
             breed='lif_soma',
             config_name='config_0',
