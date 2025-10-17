9e8246df8e1c9a6a95593ee65cbf040565460fba
wfishell <wf2322@columbia.edu>
2025-10-17 16:24:24 -0400

    lzy commit msg

--- superneuroabm/ssn/NMNIST-test.py
+++ superneuroabm/ssn/NMNIST-test.py
@@ -197,29 +197,38 @@ class Conv2dtSingleLayer:
 
 
 if __name__ == '__main__':
-
+    #Create Convolutionary NN
     Model = Conv2dtSingleLayer()
     Model.model.setup(use_gpu=True)
+    #Convolutionary Filters
     Model.Conv_Kernel_Construction(3, 2, layer_idx=0)
     Model.Conv_Kernel_Construction(4, 4, layer_idx=0)
     Model.Conv_Kernel_Construction(4, 2, layer_idx=0)
     Model.Conv_Kernel_Construction(5, 5, layer_idx=0)
+    #TODO add filter matrix to set the thresholds and synaptic weights
+    #Each Conv channel has one output neuron
+    #Computing combanitoric set
     Model.AttentionPooling(0,4,4)
     Dataset = Model.load_bin_as_spike_dict('/lustre/orion/proj-shared/lrn088/objective3/wfishell/superneuroabm/superneuroabm/ssn/data/NMNIST/Test/1/00003.bin')
+    #Dataset Time, X,Y,=> compute spatial hashing spike=suzdkik(x,y)
+    #Dataset sample {T1:{(x,y):spike value, (x_1,y_1): spike value 2 ....}, t2 ,...}
     for time in Dataset:
         for kernel_array, kernel_neuron in Model.layers[0]:
                 for spike in Dataset[time]:
                     Model.Convolve_Spike(spike, kernel_array, Dataset[time], time, 1)
     
     Spike_Times={}
-    Model.model.simulate(ticks=3000, update_data_ticks=3000)
+    Model.model.simulate(ticks=1300, update_data_ticks=1300)
 
     for index_i in range(len(Model.pooling_matrix[0])):
         for index_j in range(len(Model.pooling_matrix[0][0])):
             if Model.pooling_matrix[0][index_i][index_j]:
                 Coor=(index_i,index_j)
                 Spike_Times[Coor]=Model.model.get_spike_times(soma_id=Model.pooling_matrix[0][index_i][index_j])
+                #{(x,y):[spike time list],....}
+    #invert spike gets us back to the dataset form seen in lines 213-214
     Spike_Times=Model.invert_dict(Spike_Times)
+    #TODO should we have spatial hashing? 
         
     Model.Conv_Kernel_Construction(3, 3, layer_idx=1)
     Model.Conv_Kernel_Construction(4, 4, layer_idx=1)
@@ -228,9 +237,6 @@ if __name__ == '__main__':
     for time in Spike_Times:
         for kernel_array, kernel_neuron in Model.layers[1]:
                 for spike in Spike_Times[time]:
-                    print(spike,'spike')
-                    print(Spike_Times[time],'Spike Times Data')
-                    print(time,'Time')
                     Model.Convolve_Spike(spike, kernel_array, Spike_Times[time], time, 1)
       
     Spike_Times_Layer2={}
@@ -239,9 +245,3 @@ if __name__ == '__main__':
             if Model.pooling_matrix[1][index_i][index_j]:
                 Coor=(index_i,index_j)
                 Spike_Times_Layer2[Coor]=Model.model.get_spike_times(soma_id=Model.pooling_matrix[1][index_i][index_j])
-
-    print('SPIKE TIMES')
-    print(Spike_Times)
-    print('SPIKE TIMES 2')
-
-    print(Spike_Times_Layer2)
